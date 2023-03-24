c485570aa8702f438d63f3dce60c23809002fb3f
giuseppesciacca <sciaccagi@gmail.com>
2023-03-24 12:12:02 +0100

    [add] array zucchine

--- Snack 1/main.js
+++ Snack 1/main.js
@@ -1,2 +1,54 @@
 /* Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.Calcola quanto pesano tutte le zucchine.  */
 
+const zucchine = [
+    {
+        varietà: 'Zucchino nero di Milano',
+        peso: 250,
+        lunghezza: 12
+    },
+    {
+        varietà: 'Zucchino romanesco',
+        peso: 230,
+        lunghezza: 15
+    },
+    {
+        varietà: 'Zucchino ortolano di Faenza',
+        peso: 180,
+        lunghezza: 13
+    },
+    {
+        varietà: 'Zucchina lunga fiorentina',
+        peso: 140,
+        lunghezza: 25
+    },
+    {
+        varietà: 'Zucchino siciliano',
+        peso: 400,
+        lunghezza: 40
+    },
+    {
+        varietà: 'Zucchina striata di Napoli',
+        peso: 90,
+        lunghezza: 16
+    },
+    {
+        varietà: 'Zucchina bianca triestina',
+        peso: 140,
+        lunghezza: 10
+    },
+    {
+        varietà: 'Zucchina rigata pugliese',
+        peso: 260,
+        lunghezza: 19
+    },
+    {
+        varietà: 'Zucchino tondo di Piacenza',
+        peso: 350,
+        lunghezza: 15
+    },
+    {
+        varietà: 'Zucchino tondo di Nizza',
+        peso: 100,
+        lunghezza: 8
+    },
+];
